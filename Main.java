4bc26bdf87b411a3aef12834b97d7f4e12adf25b
s1shamsi <eng.saidj@gmail.com>
2022-12-24 23:59:56 -0800

    add comment for understanding the code

--- Main.java
+++ Main.java
@@ -26,7 +26,9 @@ public class Main {
 		school1.setLocation(sc.nextLine());
 		System.out.println("\n");
 		System.out.println("\n");
-		while(option) {
+		
+		
+		while(option) {  //loop start with add department 
 	
 			Department dep = new Department();
 			System.out.print("Please Enter Department Name : ");
@@ -34,14 +36,16 @@ public class Main {
 			System.out.print("Please Enter Department Floor : ");
 			dep.setNumOfFloors(sc.nextInt());
 			
-			while(option2) {
+			while(option2) { //loop 1 in loop to add teacher
 				
 				Teacher t = new Teacher();
 				System.out.print("Please Enter Teacher name : ");
 				t.setNameOfTeacher(sc.next());
 				System.out.print("Please Enter Teacher ID : ");
 				t.setIdOfTeacher(sc.nextInt());
-				while(option3) {
+				
+				while(option3) {                           //loop2 in loop 1 for add course
+					
 					Student st = new Student();
 					System.out.print("Please Enter Student name : ");
 					st.setNameOfStudent(sc.next());
@@ -50,7 +54,7 @@ public class Main {
 			
 					t.studentList.add(st);
 				
-				    while(option4) {
+				    while(option4) {                        //loop3 in loop 2 for add course 
 					
 					Course cr = new Course();
 					System.out.print("Please Enter Course name : ");
@@ -60,14 +64,14 @@ public class Main {
 					System.out.print("Please Enter Mark : ");
 					cr.markCourse.setTheMark(sc.nextInt());
 					st.coList.add(cr);
-					System.out.print("Press 1 to add course, Press 2 if you dont want to add course ");
+					System.out.print("Press 1 to add course, Press 2 if you dont want to add course "); //repeat or end the course loop
 					if(sc.nextInt()!= 1) {
 						option4 = false;
 					}
 					
 					
 				}
-				    System.out.print("Press 1 to add student, Press 2 if you dont want to add student ");
+				    System.out.print("Press 1 to add student, Press 2 if you dont want to add student ");//repeat or end the student loop
 					if(sc.nextInt()!= 1) {
 						option3 = false;
 					
@@ -78,7 +82,7 @@ public class Main {
 				
 				dep.arr2.add(t);
 				
-				System.out.print("Press 1 to add Teacher, Press 2 if you dont want to add Teacher");
+				System.out.print("Press 1 to add Teacher, Press 2 if you dont want to add Teacher");//repeat or end the teacher loop
 				option3 = true;
 				if(sc.nextInt()!= 1) {
 					option2 = false;
@@ -86,9 +90,10 @@ public class Main {
 				}
 			}
 			dep1.add(dep);
-			System.out.print("Please Enter 1 to add more departments 2 to print the report");
+			System.out.print("Please Enter 1 to add more departments 2 to print the report"); //repeat or end the department loop or print the report
 			option2 = true;
-			if(sc.nextInt()!= 1) {
+			
+			if(sc.nextInt()!= 1) { //print school details 
 				option = false;
 				System.out.println("\n");
 				System.out.println("============================================");
@@ -101,24 +106,24 @@ public class Main {
 				System.out.println(" School Location is :" + school1.getLocation());
 				System.out.println("===============  Department  =================");
 
-				for( Department department :dep1) {
+				for( Department department :dep1) { // print department details 
 					
 					System.out.println(" Department name is " + department.getNameOfDepatment());
 					System.out.println(" Department Floor No. is " + department.getNumOfFloors());
 					System.out.println("===============     Teachers  =================");
-					for(Teacher t : department.arr2) {
+					for(Teacher t : department.arr2) { //print teacher details 
 						  System.out.println("===============   Teacher Details   =================");
 						System.out.println(" Teacher name is : " + t.getNameOfTeacher());
 						System.out.println(" Teacher id is : :" + t.getIdOfTeacher());
 						System.out.println("===============   Students   =================");
-						for(Student s : t.studentList) {
+						for(Student s : t.studentList) { //print student details 
 							//System.out.println("================== Student Details =====================");
 							System.out.println(" Student name is : " + s.getNameOfStudent());
 							System.out.println(" Student id is : :" + s.getIdOfStudent());
 							
 							System.out.println("================================================");
 							  System.out.println("===============   Courses   =================");
-							  for(Course c : s.coList) {
+							  for(Course c : s.coList) { //print course details and mark 
 								
 								  System.out.println("===============   Course Details  =================");
 								System.out.println(" Course name is : " + c.getNameOfCourse());
@@ -132,7 +137,7 @@ public class Main {
 					
 					System.out.println("================================================");
 				}
-			
+			///athkar ending
 				System.out.println("================================================");
 				System.out.println("======= Thank you for using our system! =======");
 				System.out.println("============== Alhamdulillah ===================");
